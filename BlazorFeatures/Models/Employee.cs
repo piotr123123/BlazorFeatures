10de5ab75191fc8e731d6c7dbd0a23a794868428
DESKTOP-G6T324R\grucha <piotr9@gmail.com>
2020-11-06 16:23:55 +0100

    Creating model calsses

---
+++ BlazorFeatures/Models/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class Employee
+    {
+        public Employee()
+        {
+
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public Gender Gender { get; set; }
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+        public string PhotoPath { get; set; }
+
+
+        public Employee(int Id, string Name, string Surname, Gender Gender, int DepartmentId, string PhotoPath)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Surname = Surname;
+            this.Gender = Gender;
+            this.DepartmentId = DepartmentId;
+            this.PhotoPath = PhotoPath;
+        }
+
+       
+    }
+}
+
