206eb2b43ec047de4640393562bb61f090f2fe86
DESKTOP-G6T324R\grucha <piotr9@gmail.com>
2020-11-22 20:33:38 +0100

    I have added DbContext and I have done update the database schema

---
+++ BlazorFeatures/EmployeeManagementApi/Models/AppDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementApi.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employeesa { get; set; }
+        public DbSet<Department> Departmentsa { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //modelBuilder.Entity<Department>().HasData(
+            //    new Department { DepartmentId = 1, DepartmentName = "it" });
+
+            //modelBuilder.Entity<Employee>().hasdata(new Employee
+            //{
+            //    Id = 1,
+            //    Name = "magdalena",
+            //    Surname = "jaskulska",
+            //    Gender = Gender.Female,
+            //    DepartmentId = 1,
+            //    photopath = "img1.jpg"
+            //});
+        }
+
+    }
+}
