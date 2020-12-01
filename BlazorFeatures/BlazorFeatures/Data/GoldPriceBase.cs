7b38bb5fd94a356fb87bcc49d5efbaa7b9a6841f
DESKTOP-G6T324R\grucha <piotr9@gmail.com>
2020-12-01 21:15:54 +0100

    GoldPrice component added

---
+++ BlazorFeatures/BlazorFeatures/Data/GoldPriceBase.cs
@@ -0,0 +1,30 @@
+﻿using BlazorFeatures.Services;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace BlazorFeatures.Data
+{
+    public class GoldPriceBase : ComponentBase
+    {
+        [Inject]
+        public IGoldPrice GoldPriceService { get; set; }
+        public double Wynik { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            //Wynik = GoldPriceService.GetGoldPrice1().Item2[0].cena; 
+        }
+
+        public double CenaZlota()
+        {
+            var cena = GoldPriceService.GetGoldPrice1().Item2[0].cena;
+
+            return cena;
+        }
+    }
+}
