64440809acc19e81c876a92882b66a54ce140ff0
DESKTOP-G6T324R\grucha <piotr9@gmail.com>
2020-12-01 21:11:23 +0100

    Gold Price model added

---
+++ BlazorFeatures/Models/GoldPrice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class GoldPrice
+    {
+        public DateTime data { get; set; }
+        public double cena { get; set; }
+
+        public GoldPrice(DateTime dateTime, double cena)
+        {
+            this.data = dateTime;
+            this.cena = cena;
+        }
+
+        public GoldPrice()
+        { 
+        
+        }
+    }
+}
