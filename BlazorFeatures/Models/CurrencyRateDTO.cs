8e372a4944cea1068d56bda7ffce4d899954fbcb
DESKTOP-G6T324R\grucha <piotr9@gmail.com>
2021-01-25 19:01:08 +0100

    CurrencyRateDTO was added

---
+++ BlazorFeatures/Models/CurrencyRateDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class CurrencyRateDTO
+    {
+        public string Table { get; set; }
+        public string No { get; set; }
+        public DateTime EffectiveDate { get; set; }
+        public Rates[] Rates { get; set; }
+    }
+
+    public class Rates
+    {
+        public string Currency { get; set; }
+        public string Code { get; set; }
+        public double Mid { get; set; }
+    }
+}
