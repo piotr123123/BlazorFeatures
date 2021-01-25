aebb8f8a6b52844f6c44ce783cabc0270df4a5df
DESKTOP-G6T324R\grucha <piotr9@gmail.com>
2021-01-25 19:53:21 +0100

    New model was added.

---
+++ BlazorFeatures/Models/CurrencyRateCollectionDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class CurrencyRateCollectionDTO
+    {
+        public string Currency { get; set; }
+        public string Code { get; set; }
+        public double Mid { get; set; }
+
+        public CurrencyRateCollectionDTO(string Currency, string Code, double Mid)
+        {
+            this.Currency = Currency;
+            this.Code = Code;
+            this.Mid = Mid;
+        }
+    }
+}
