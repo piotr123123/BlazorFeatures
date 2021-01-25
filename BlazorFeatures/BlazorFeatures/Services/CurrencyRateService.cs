bd5f1ae78c884f6ac0b479537e1fcb5611a30e31
DESKTOP-G6T324R\grucha <piotr9@gmail.com>
2021-01-25 19:20:04 +0100

    Currency Rate Interface and Service were added

---
+++ BlazorFeatures/BlazorFeatures/Services/CurrencyRateService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BlazorFeatures.Services
+{
+    public class CurrencyRateService : ICurrencyRate
+    {
+        private readonly HttpClient httpClient;
+        public CurrencyRateService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public CurrencyRateService()
+        {
+
+        }
+        public async Task<IEnumerable<CurrencyRateDTO>> GetListOfCurrency()
+        {
+            return await httpClient.GetJsonAsync<CurrencyRateDTO[]>("api/exchangerates/tables/a/?format=json");
+        }
+    }
+}
